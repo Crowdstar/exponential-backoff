2ce611ead8c3689173c5f541ae2d173d3083d8d3
Demin Yin <demin.yin@glu.com>
2021-12-10 12:08:44 -0800

    backward-compatible changes allowing doing exponential backoff when multiple throwable objects are thrown out

--- src/ExceptionBasedCondition.php
+++ src/ExceptionBasedCondition.php
@@ -26,26 +26,26 @@ use Throwable;
 
 /**
  * Class ExceptionBasedCondition
- * Do a retry if specified type of exception is thrown out.
+ * Do a retry if specified types of exceptions are thrown out.
  *
  * @package CrowdStar\Backoff
  */
 class ExceptionBasedCondition extends AbstractRetryCondition
 {
     /**
-     * @var string
+     * @var string[]
      */
-    protected $exception;
+    protected $exceptions;
 
     /**
      * ExceptionBasedCondition constructor.
      *
-     * @param string $exception
      * @throws Exception
      */
-    public function __construct(string $exception = \Exception::class)
+    public function __construct(string ...$exceptions)
     {
-        $this->setException($exception);
+        $exceptions = $exceptions ?: [BaseException::class];
+        $this->setExceptions(...$exceptions);
     }
 
     /**
@@ -60,39 +60,59 @@ class ExceptionBasedCondition extends AbstractRetryCondition
 
     /**
      * @return string
+     * @deprecated This will be removed in the next major version. Use {@see self::getExceptions} instead.
+     * @see \CrowdStar\Backoff\ExceptionBasedCondition::getExceptions()
      */
     public function getException(): string
     {
-        return $this->exception;
+        if (count($this->exceptions) === 1) {
+            return $this->exceptions[0];
+        }
+
+        throw new Exception('Method ' . __METHOD__ . ' can be used only when one type of exception to be handled.');
     }
 
     /**
-     * @param string $exception
-     * @return ExceptionBasedCondition
+     * @return string[]
+     */
+    public function getExceptions(): array
+    {
+        return $this->exceptions;
+    }
+
+    /**
      * @throws Exception
+     * @deprecated This will be removed in the next major version. Use {@see self::setExceptions} instead.
+     * @see \CrowdStar\Backoff\ExceptionBasedCondition::setExceptions()
      */
     public function setException(string $exception): self
     {
-        if (class_exists($exception)) {
-            $class = new ReflectionClass($exception);
-            if ((BaseException::class != $class->getName()) && !$class->isSubclassOf(BaseException::class)) {
-                throw new Exception("{$exception} objects are not instances of class \Exception");
+        return $this->setExceptions($exception);
+    }
+
+    public function setExceptions(string ...$exceptions): self
+    {
+        $this->exceptions = [];
+        foreach ($exceptions as $exception) {
+            if (!class_exists($exception) && !interface_exists($exception)) {
+                throw new Exception("Class/interface \"{$exception}\" does not exist");
             }
 
-            $this->exception = $exception;
-
-            return $this;
-        } elseif (interface_exists($exception)) {
             $class = new ReflectionClass($exception);
-            if ((Throwable::class != $class->getName()) && !$class->implementsInterface(Throwable::class)) {
-                throw new Exception("{$exception} objects are not instances of interface \Throwable");
+
+            if (class_exists($exception)) {
+                if ((BaseException::class != $class->getName()) && !$class->isSubclassOf(BaseException::class)) {
+                    throw new Exception("{$exception} objects are not instances of class \Exception");
+                }
+            } else {
+                if ((Throwable::class != $class->getName()) && !$class->implementsInterface(Throwable::class)) {
+                    throw new Exception("{$exception} objects are not instances of interface \Throwable");
+                }
             }
 
-            $this->exception = $exception;
-
-            return $this;
+            $this->exceptions[] = $exception;
         }
 
-        throw new Exception("Class/interface \"{$exception}\" does not exist");
+        return $this;
     }
 }
